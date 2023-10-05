2184b75062935512f3526ea395770d77deb04cc7
myhomebox <136135963+myhomebox@users.noreply.github.com>
2023-10-05 10:47:39 +0800

    Update Panghuys.js

--- js/Panghuys.js
+++ js/Panghuys.js
@@ -3,9 +3,10 @@
 var rule = {
 	title:'胖虎',
 	//模板:'mxpro',
-	host:'http://www.panghuys.com',
-	//host:'https://my.cdnkan.top/',
+	//host:'http://www.panghuys.com',
+	host:'https://my.cdnkan.top/',
 	//hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(0)&&a&&href");print(src);HOST=src',
+	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(1)&&a&&href");print(src);HOST=src',
 	url:'/vodshow/fyclassfyfilter.html',
 	filterable:1,//是否启用分类筛选,
 	filter_url:'{{fl.area}}{{fl.by or "/by/time"}}{{fl.class}}{{fl.lang}}{{fl.letter}}/page/fypage{{fl.year}}',
