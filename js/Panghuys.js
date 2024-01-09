58dd765adb733e7cab32b881589d921a52557bb7
myhomebox <136135963+myhomebox@users.noreply.github.com>
2024-01-09 11:58:39 +0800

    Update Panghuys.js

--- js/Panghuys.js
+++ js/Panghuys.js
@@ -4,7 +4,7 @@ var rule = {
 	title:'胖虎',
 	//模板:'mxpro',
 	//host:'http://www.panghuys.com',
-	host:'https://my.cdnkan.top/',
+	host:'https://www.cdnkan.top/',
 	//hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(0)&&a&&href");print(src);HOST=src',
 	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(1)&&a&&href");print(src);HOST=src',
 	url:'/vodshow/fyclassfyfilter.html',
