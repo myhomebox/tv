acc29bb0a237b86c9eeea46a1258dad91c365c41
myhomebox <136135963+myhomebox@users.noreply.github.com>
2024-01-15 00:09:25 +0800

    Update mimiju.js

--- js/mimiju.js
+++ js/mimiju.js
@@ -6,27 +6,31 @@ var rule = {
     searchable: 1, //是否启用全局搜索,
     quickSearch: 1, //是否启用快速搜索,
     filterable: 1, //是否启用分类筛选,
-    filter_url: '{{fl.cateId}}--{{by}}------fypage---.html',
+    filter_url: '{{fl.cateId}}--{{by}}---{{fl.letter}}---fypage---.html',
     filter: {
-    	"20":[{"key":"by","name":"排序","value":[{"n":"全部","v":""},{"n":"按時間","v":"time"},{"n":"按人氣","v":"hits"},{"n":"按評分","v":"score"}]}],
-        "21":[{"key":"by","name":"排序","value":[{"n":"全部","v":""},{"n":"按時間","v":"time"},{"n":"按人氣","v":"hits"},{"n":"按評分","v":"score"}]}],
-        "22":[{"key":"by","name":"排序","value":[{"n":"全部","v":""},{"n":"按時間","v":"time"},{"n":"按人氣","v":"hits"},{"n":"按評分","v":"score"}]}],
-        "23":[{"key":"by","name":"排序","value":[{"n":"全部","v":""},{"n":"按時間","v":"time"},{"n":"按人氣","v":"hits"},{"n":"按評分","v":"score"}]}],
-        "24":[{"key":"by","name":"排序","value":[{"n":"全部","v":""},{"n":"按時間","v":"time"},{"n":"按人氣","v":"hits"},{"n":"按評分","v":"score"}]}]
-    },
+    	"20":[{"key":"letter","name":"字母","value":[{"n":"全部","v":""},{"n":"A","v":"A"},{"n":"B","v":"B"},{"n":"C","v":"C"},{"n":"D","v":"D"},{"n":"E","v":"E"},{"n":"F","v":"F"},{"n":"G","v":"G"},{"n":"H","v":"H"},{"n":"I","v":"I"},{"n":"J","v":"J"},{"n":"K","v":"K"},{"n":"L","v":"L"},{"n":"M","v":"M"},{"n":"N","v":"N"},{"n":"O","v":"O"},{"n":"P","v":"P"},{"n":"Q","v":"Q"},{"n":"R","v":"R"},{"n":"S","v":"S"},{"n":"T","v":"T"},{"n":"U","v":"U"},{"n":"V","v":"V"},{"n":"W","v":"W"},{"n":"X","v":"X"},{"n":"Y","v":"Y"},{"n":"Z","v":"Z"},{"n":"0-9","v":"0-9"}]},
+    		  {"key":"by","name":"排序","value":[{"n":"全部","v":""},{"n":"按時間","v":"time"},{"n":"按人氣","v":"hits"},{"n":"按評分","v":"score"}]}],
+    	"21":[{"key":"letter","name":"字母","value":[{"n":"全部","v":""},{"n":"A","v":"A"},{"n":"B","v":"B"},{"n":"C","v":"C"},{"n":"D","v":"D"},{"n":"E","v":"E"},{"n":"F","v":"F"},{"n":"G","v":"G"},{"n":"H","v":"H"},{"n":"I","v":"I"},{"n":"J","v":"J"},{"n":"K","v":"K"},{"n":"L","v":"L"},{"n":"M","v":"M"},{"n":"N","v":"N"},{"n":"O","v":"O"},{"n":"P","v":"P"},{"n":"Q","v":"Q"},{"n":"R","v":"R"},{"n":"S","v":"S"},{"n":"T","v":"T"},{"n":"U","v":"U"},{"n":"V","v":"V"},{"n":"W","v":"W"},{"n":"X","v":"X"},{"n":"Y","v":"Y"},{"n":"Z","v":"Z"},{"n":"0-9","v":"0-9"}]},
+    		  {"key":"by","name":"排序","value":[{"n":"全部","v":""},{"n":"按時間","v":"time"},{"n":"按人氣","v":"hits"},{"n":"按評分","v":"score"}]}],
+    	"22":[{"key":"letter","name":"字母","value":[{"n":"全部","v":""},{"n":"A","v":"A"},{"n":"B","v":"B"},{"n":"C","v":"C"},{"n":"D","v":"D"},{"n":"E","v":"E"},{"n":"F","v":"F"},{"n":"G","v":"G"},{"n":"H","v":"H"},{"n":"I","v":"I"},{"n":"J","v":"J"},{"n":"K","v":"K"},{"n":"L","v":"L"},{"n":"M","v":"M"},{"n":"N","v":"N"},{"n":"O","v":"O"},{"n":"P","v":"P"},{"n":"Q","v":"Q"},{"n":"R","v":"R"},{"n":"S","v":"S"},{"n":"T","v":"T"},{"n":"U","v":"U"},{"n":"V","v":"V"},{"n":"W","v":"W"},{"n":"X","v":"X"},{"n":"Y","v":"Y"},{"n":"Z","v":"Z"},{"n":"0-9","v":"0-9"}]},
+    		  {"key":"by","name":"排序","value":[{"n":"全部","v":""},{"n":"按時間","v":"time"},{"n":"按人氣","v":"hits"},{"n":"按評分","v":"score"}]}],
+    	"23":[{"key":"letter","name":"字母","value":[{"n":"全部","v":""},{"n":"A","v":"A"},{"n":"B","v":"B"},{"n":"C","v":"C"},{"n":"D","v":"D"},{"n":"E","v":"E"},{"n":"F","v":"F"},{"n":"G","v":"G"},{"n":"H","v":"H"},{"n":"I","v":"I"},{"n":"J","v":"J"},{"n":"K","v":"K"},{"n":"L","v":"L"},{"n":"M","v":"M"},{"n":"N","v":"N"},{"n":"O","v":"O"},{"n":"P","v":"P"},{"n":"Q","v":"Q"},{"n":"R","v":"R"},{"n":"S","v":"S"},{"n":"T","v":"T"},{"n":"U","v":"U"},{"n":"V","v":"V"},{"n":"W","v":"W"},{"n":"X","v":"X"},{"n":"Y","v":"Y"},{"n":"Z","v":"Z"},{"n":"0-9","v":"0-9"}]},
+    		  {"key":"by","name":"排序","value":[{"n":"全部","v":""},{"n":"按時間","v":"time"},{"n":"按人氣","v":"hits"},{"n":"按評分","v":"score"}]}],
+    	"24":[{"key":"letter","name":"字母","value":[{"n":"全部","v":""},{"n":"A","v":"A"},{"n":"B","v":"B"},{"n":"C","v":"C"},{"n":"D","v":"D"},{"n":"E","v":"E"},{"n":"F","v":"F"},{"n":"G","v":"G"},{"n":"H","v":"H"},{"n":"I","v":"I"},{"n":"J","v":"J"},{"n":"K","v":"K"},{"n":"L","v":"L"},{"n":"M","v":"M"},{"n":"N","v":"N"},{"n":"O","v":"O"},{"n":"P","v":"P"},{"n":"Q","v":"Q"},{"n":"R","v":"R"},{"n":"S","v":"S"},{"n":"T","v":"T"},{"n":"U","v":"U"},{"n":"V","v":"V"},{"n":"W","v":"W"},{"n":"X","v":"X"},{"n":"Y","v":"Y"},{"n":"Z","v":"Z"},{"n":"0-9","v":"0-9"}]},
+    		  {"key":"by","name":"排序","value":[{"n":"全部","v":""},{"n":"按時間","v":"time"},{"n":"按人氣","v":"hits"},{"n":"按評分","v":"score"}]}]
+             },
     headers: { //网站的请求头,完整支持所有的,常带ua和cookies
         'User-Agent': 'MOBILE_UA',
         // "Cookie": "searchneed=ok"
     },
     filter_def: {
-        20: {cateId: '20'},
+        20:{cateId: '20'},
         21:{cateId:'21'},
 	22:{cateId:'22'},
-	23:{cateId:'23'},
-        24:{cateId:'24'}
+	23:{cateId:'23'}
     },
-    class_name: '短劇&電視劇&電影&動漫&綜藝',
-    class_url: '20&21&22&23&24',
+    class_name: '電視劇&電影&動漫&綜藝',
+    class_url: '20&21&22&23',
     play_parse: true,
    lazy: `js:
         var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
@@ -43,16 +47,16 @@ var rule = {
         }
     `,
     limit: 6,
-    推荐: 'ul.hl-vod-list;li.hl-list-item;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+    推荐: 'ul.myui-vodlist;li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
     double: true, // 推荐内容是否双层定位
-    一级: 'ul.hl-vod-list li.hl-list-item;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+    一级: 'ul.myui-vodlist li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
     二级: {
-        "title":"h2&&Text;li.hl-col-xs-12:eq(6)--em&&Text",
-        "img":".hl-lazy&&data-original",
-        "desc":"li.hl-col-xs-12:eq(1)--em&&Text;li.hl-col-xs-12:eq(4)--em&&Text;li.hl-col-xs-12:eq(5)--em&&Text;li.hl-col-xs-12:eq(2)--em&&Text;li.hl-col-xs-12:eq(3)--em&&Text",
-        "content":"li.hl-col-xs-12:eq(11)--em&&Text",
-        "tabs":".hl-from-list li",
-        "lists":"#hl-plays-list:eq(#id) li"
+        "title":"h1&&Text;p.data:eq(0)&&a:eq(0)&&Text",
+        "img":".lazyload&&data-original",
+        "desc":"p.data:eq(1)&&Text;p.data:eq(0)&&a:eq(2)&&Text;p.data:eq(0)&&a:eq(1)&&Text;p.data:eq(2)--span&&Text;p.data:eq(3)--span&&Text",
+        "content":"span.data&&Text",
+        "tabs":"ul.nav-tabs li",
+        "lists":"ul.myui-content__list:eq(#id) li"
     },
-    搜索: 'body .hl-one-list&&.hl-list-item;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+    搜索: 'ul.searchList li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
 }
