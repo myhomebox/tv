78cea43a330b36f43d19497cd8219f70cb381e73
myhomebox <136135963+myhomebox@users.noreply.github.com>
2024-12-09 12:35:03 +0800

    Update 99itv.js

--- js/99itv.js
+++ js/99itv.js
@@ -37,7 +37,7 @@ var rule = {
     class_name: '電影&電視劇&綜藝&動漫',
     class_url: 'movie&drama&variety&anime',
     play_parse: true,
-    	lazy:`js:
+    lazy:`js:
 		var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
 		var url = html.url;
 		if (html.encrypt == '1') {
@@ -45,13 +45,11 @@ var rule = {
 		} else if (html.encrypt == '2') {
 			url = unescape(base64Decode(url))
 		}
-		let play_Url = 'json:http://127.0.0.1:10079/parse/?thread=0&proxy=&url=';
 		if (/\\.m3u8|\\.mp4/.test(url)) {
 			input = {
 				jx: 0,
 				url: url,
-				playUrl: play_Url,
-				parse: 1
+				parse: 0
 			}
 		} else if (/\\/share/.test(url)) {
 			url = getHome(url) + request(url).match(/main.*?"(.*?)"/)[1];
