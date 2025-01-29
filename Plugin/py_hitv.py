07db0f30d24777bc2b53d96a488c7dd99b50937d
myhomebox <136135963+myhomebox@users.noreply.github.com>
2025-01-29 15:21:22 +0800

    Update py_hitv.py

--- Plugin/py_hitv.py
+++ Plugin/py_hitv.py
@@ -47,7 +47,7 @@ class Spider(Spider):
         result['class'] = classes
         return result
 
-    host = "https://hitv.ai"
+    host = "https://wys.upfuhn.com"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/80.0.3987.149 Safari/537.36"
