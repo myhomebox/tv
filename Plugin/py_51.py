4bf51b5aa87b3b69a8d8df7652002708125410da
myhomebox <136135963+myhomebox@users.noreply.github.com>
2025-03-28 16:13:42 +0800

    Update py_51.py

--- Plugin/py_51.py
+++ Plugin/py_51.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 # by @嗷呜
 import json
@@ -6,7 +7,7 @@ import re
 import sys
 import threading
 import time
-from base64 import b64decode
+from base64 import b64decode, b64encode
 import requests
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
@@ -18,7 +19,24 @@ from base.spider import Spider
 class Spider(Spider):
 
     def init(self, extend=""):
+        try:self.proxies = json.loads(extend)
+        except:self.proxies = {}
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="134", "Google Chrome";v="134"',
+            'DNT': '1',
+            'sec-ch-ua-mobile': '?0',
+            'Origin': '',
+            'Sec-Fetch-Site': 'cross-site',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+        }
         self.host=self.host_late(self.get_domains())
+        self.headers.update({'Origin': self.host, 'Referer': f"{self.host}/"})
         pass
 
     def getName(self):
@@ -33,24 +51,8 @@ class Spider(Spider):
     def destroy(self):
         pass
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="134", "Google Chrome";v="134"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'dnt': '1',
-        'upgrade-insecure-requests': '1',
-        'sec-fetch-site': 'cross-site',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'accept-language': 'zh-CN,zh;q=0.9',
-        'priority': 'u=0, i'
-    }
-
     def homeContent(self, filter):
-        data=self.getpq(self.fetch(self.host, headers=self.headers).text)
+        data=self.getpq(requests.get(self.host, headers=self.headers,proxies=self.proxies).text)
         result = {}
         classes = []
         for k in data('.category-list ul li').items():
@@ -66,7 +68,7 @@ class Spider(Spider):
         pass
 
     def categoryContent(self, tid, pg, filter, extend):
-        data=self.getpq(self.fetch(f"{self.host}{tid}{pg}", headers=self.headers).text)
+        data=self.getpq(requests.get(f"{self.host}{tid}{pg}", headers=self.headers,proxies=self.proxies).text)
         result = {}
         result['list'] = self.getlist(data('#archive article a'))
         result['page'] = pg
@@ -77,12 +79,8 @@ class Spider(Spider):
 
     def detailContent(self, ids):
         url=f"{self.host}{ids[0]}"
-        data=self.getpq(self.fetch(url, headers=self.headers).text)
-        vod = {
-            'vod_content': data('.post-title').text(),
-            'vod_play_from': '51吸瓜',
-            'vod_play_url': f"请停止活塞运动，可能没有视频${url}"
-        }
+        data=self.getpq(requests.get(url, headers=self.headers,proxies=self.proxies).text)
+        vod = {'vod_play_from': '51吸瓜'}
         try:
             clist = []
             if data('.tags .keywords a'):
@@ -92,7 +90,7 @@ class Spider(Spider):
                     clist.append('[a=cr:' + json.dumps({'id': href, 'name': title}) + '/]' + title + '[/a]')
             vod['vod_content'] = ' '.join(clist)
         except:
-            pass
+            vod['vod_content'] = data('.post-title').text()
         try:
             plist=[]
             if data('.dplayer'):
@@ -101,43 +99,91 @@ class Spider(Spider):
                     plist.append(f"视频{c}${config['video']['url']}")
             vod['vod_play_url']='#'.join(plist)
         except:
-            pass
+            vod['vod_play_url']=f"请停止活塞运动，可能没有视频${url}"
         return {'list':[vod]}
 
     def searchContent(self, key, quick, pg="1"):
-        data=self.getpq(self.fetch(f"{self.host}/search/{key}/{pg}", headers=self.headers).text)
+        data=self.getpq(requests.get(f"{self.host}/search/{key}/{pg}", headers=self.headers,proxies=self.proxies).text)
         return {'list':self.getlist(data('#archive article a')),'page':pg}
 
     def playerContent(self, flag, id, vipFlags):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
-            'Pragma': 'no-cache',
-            'Cache-Control': 'no-cache',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="134", "Google Chrome";v="134"',
-            'DNT': '1',
-            'sec-ch-ua-mobile': '?0',
-            'Origin': self.host,
-            'Sec-Fetch-Site': 'cross-site',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Dest': 'empty',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-        }
-        return  {'parse': 1, 'url': id, 'header': headers}
+        p=1
+        if '.m3u8' in id:p,id=0,self.proxy(id)
+        return  {'parse': p, 'url': id, 'header': self.headers}
 
     def localProxy(self, param):
-        res=self.fetch(param['url'], headers=self.headers, timeout=10)
-        return [200,res.headers.get('Content-Type'),self.aesimg(res.content)]
+        if param.get('type') == 'img':
+            res=requests.get(param['url'], headers=self.headers, proxies=self.proxies, timeout=10)
+            return [200,res.headers.get('Content-Type'),self.aesimg(res.content)]
+        elif param.get('type') == 'm3u8':return self.m3Proxy(param['url'])
+        else:return self.tsProxy(param['url'])
+
+    def proxy(self, data, type='m3u8'):
+        if data and len(self.proxies):return f"{self.getProxyUrl()}&url={self.e64(data)}&type={type}"
+        else:return data
+
+    def m3Proxy(self, url):
+        url=self.d64(url)
+        ydata = requests.get(url, headers=self.headers, proxies=self.proxies, allow_redirects=False)
+        data = ydata.content.decode('utf-8')
+        if ydata.headers.get('Location'):
+            url = ydata.headers['Location']
+            data = requests.get(url, headers=self.headers, proxies=self.proxies).content.decode('utf-8')
+        lines = data.strip().split('\n')
+        last_r = url[:url.rfind('/')]
+        if len(lines) < 10:
+            filtered = [item for item in lines if '#EXT' not in item and item.strip()]
+            if len(filtered) == 1:
+                u = last_r + ('' if filtered[0].startswith('/') else '/') + filtered[0]
+                data = requests.get(u, headers=self.headers, proxies=self.proxies).content.decode('utf-8')
+                lines = data.strip().split('\n')
+        iskey=True
+        for index, string in enumerate(lines):
+            if iskey and 'URI' in string:
+                pattern = r'URI="([^"]*)"'
+                match = re.search(pattern, string)
+                if match:
+                    lines[index] = re.sub(pattern, f'URI="{self.proxy(match.group(1), "mkey")}"', string)
+                    iskey=False
+                    continue
+            if '#EXT' not in string and 'http' not in string:
+                line = last_r + ('' if string.startswith('/') else '/') + string
+                lines[index] = self.proxy(line, 'ts')
+        data = '\n'.join(lines)
+        return [200, "application/vnd.apple.mpegur", data]
+
+    def tsProxy(self, url):
+        url = self.d64(url)
+        data = requests.get(url, headers=self.headers, proxies=self.proxies, stream=True)
+        return [200, data.headers['Content-Type'], data.content]
+
+    def e64(self, text):
+        try:
+            text_bytes = text.encode('utf-8')
+            encoded_bytes = b64encode(text_bytes)
+            return encoded_bytes.decode('utf-8')
+        except Exception as e:
+            print(f"Base64编码错误: {str(e)}")
+            return ""
+
+    def d64(self, encoded_text):
+        try:
+            encoded_bytes = encoded_text.encode('utf-8')
+            decoded_bytes = b64decode(encoded_bytes)
+            return decoded_bytes.decode('utf-8')
+        except Exception as e:
+            print(f"Base64解码错误: {str(e)}")
+            return ""
 
     def get_domains(self):
-        html = self.getpq(self.fetch("https://51cg.fun", headers=self.headers).text)
+        html = self.getpq(requests.get("https://51cg.fun", headers=self.headers,proxies=self.proxies).text)
         html_pattern = r"Base64\.decode\('([^']+)'\)"
         html_match = re.search(html_pattern, html('script').eq(-1).text(), re.DOTALL)
         if not html_match:
-            raise Exception("未找到html_match")
+            raise Exception("未找到html")
         html = b64decode(html_match.group(1)).decode()
         words_pattern = r"words\s*=\s*'([^']+)'"
-        words_match = re.search(words_pattern, html)
+        words_match = re.search(words_pattern, html, re.DOTALL)
         if not words_match:
             raise Exception("未找到words")
         words = words_match.group(1).split(',')
@@ -168,7 +214,7 @@ class Spider(Spider):
         def test_host(url):
             try:
                 start_time = time.time()
-                response = requests.head(url, timeout=1.0, allow_redirects=False)
+                response = requests.head(url,proxies=self.proxies,timeout=1.0, allow_redirects=False)
                 delay = (time.time() - start_time) * 1000
                 results[url] = delay
             except Exception as e:
