f9a1080c1891f832ef42d0a859541169586f3bf0
myhomebox <136135963+myhomebox@users.noreply.github.com>
2025-06-10 23:18:19 +0800

    Update py_czzy.py

--- Plugin/py_czzy.py
+++ Plugin/py_czzy.py
@@ -14,7 +14,7 @@ class Spider(Spider):
         return "厂长资源"
     
     def init(self, extend=""):
-        self.host = "https://www.czzy77.com"
+        self.host = "https://www.czzyvideo.com"
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
