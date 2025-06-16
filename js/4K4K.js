3a45fdaf378fa3a1734215a5d2465ffc32f4387b
myhomebox <136135963+myhomebox@users.noreply.github.com>
2025-06-16 23:19:29 +0800

    Update 4K4K.js

--- js/4K4K.js
+++ js/4K4K.js
@@ -16,7 +16,7 @@ var rule = {
 		3:{cateId:'3'},
 		4:{cateId:'4'}
 	},
-	headers: {'User-Agent': 'MOBILE_UA'},
+	headers: {'User-Agent': 'Dart/2.14 (dart:io)'},
 	class_name:'电影&电视剧&综艺&动漫', 
 	class_url:'1&2&3&4',
 	play_parse: true,
@@ -40,7 +40,7 @@ var rule = {
 	`,
 	limit: 6,
 	推荐: '*',
-	double: false, // 推荐内容是否双层定位
+	double: true, // 推荐内容是否双层定位
         一级:'body a.module-poster-item.module-item;a&&title;a.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
         二级: {
                 "title":"h1&&Text;.module-info-tag-link:eq(2)&&Text",
