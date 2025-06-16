62cc5b7b8b302a68c80623371e66164197a08637
myhomebox <136135963+myhomebox@users.noreply.github.com>
2025-06-16 21:06:02 +0800

    Update 4K4K.js

--- js/4K4K.js
+++ js/4K4K.js
@@ -1,6 +1,5 @@
 muban.mxpro.二级.title = 'h1&&Text;.module-info-tag-link:eq(2)&&Text';
 muban.mxpro.二级.desc = '.module-info-item:eq(4)&&Text;.module-info-tag-link:eq(0)&&Text;.module-info-tag-link:eq(1)&&Text;.module-info-item-content:eq(1)&&Text;.module-info-item-content:eq(0)&&Text';
-muban.mxpro.二级.tabs = '.module-tab-item';
 muban.mxpro.二级.tab_text = 'body--small&&Text';
 var rule = {
 	title:'4K剧院', //原91free
