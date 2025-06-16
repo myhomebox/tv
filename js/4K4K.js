83b8f69d3cdc10424bc56304bf64eabbff173435
myhomebox <136135963+myhomebox@users.noreply.github.com>
2025-06-16 20:54:17 +0800

    Update 4K4K.js

--- js/4K4K.js
+++ js/4K4K.js
@@ -1,7 +1,7 @@
 var rule = {
 	title:'4K剧院', 
 	host:'https://4k4k.live',
-	url:'/vodshow/fyfilter.html',
+	url:'/vodshow/fyfilter',
 	filterable:1,//是否启用分类筛选,
 	filter_url:'{{fl.cateId}}-{{fl.area}}-{{fl.by or "time"}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
 	filter: {
