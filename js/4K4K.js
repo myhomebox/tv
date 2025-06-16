45d7932b8e3645a7c83f4b20355c406f7108b0f1
myhomebox <136135963+myhomebox@users.noreply.github.com>
2025-06-16 23:11:12 +0800

    Update 4K4K.js

--- js/4K4K.js
+++ js/4K4K.js
@@ -41,7 +41,7 @@ var rule = {
 	limit: 6,
 	推荐: '*',
 	double: false, // 推荐内容是否双层定位
-        一级:'body a.module-poster-item.module-item;a&&title;.lazyloaded&&data-src;.module-item-note&&Text;a&&href',
+        一级:'body a.module-poster-item.module-item;a&&title;a.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
         二级: {
                 "title":"h1&&Text;.module-info-tag-link:eq(2)&&Text",
                 "img":".lazyload&&data-original",
