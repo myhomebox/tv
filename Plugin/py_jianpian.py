9f28b4bd4ce436dc7a543e4ec852d4352c894430
myhomebox <136135963+myhomebox@users.noreply.github.com>
2025-07-02 01:21:06 +0800

    Update py_jianpian.py

--- Plugin/py_jianpian.py
+++ Plugin/py_jianpian.py
@@ -96,7 +96,7 @@ class Spider(Spider):
     def homeVideoContent(self):
         pass
     def categoryContent(self, tid, pg, filter, extend):
-        params={**{'fcate_pid': tid}, **{'page': pg}, **extend}
+        params={**{'fcate_pid': tid, 'page': pg}, **extend}
         path= '/api/crumb/shortList' if tid=='67' else '/api/crumb/list'
         data=self.fetch(f"{self.host}{path}",params=params,headers=self.headers).json()
         result = {}
