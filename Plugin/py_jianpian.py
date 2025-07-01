5e3e22d6e21c5ccf60114f0fc4d51e9fb002a0a8
myhomebox <136135963+myhomebox@users.noreply.github.com>
2025-07-02 00:12:21 +0800

    Update py_jianpian.py

--- Plugin/py_jianpian.py
+++ Plugin/py_jianpian.py
@@ -1,25 +1,20 @@
+# -*- coding: utf-8 -*-
+# by @嗷呜
+import concurrent.futures
+import json
 import sys
-import requests
 sys.path.append('..')
 from base.spider import Spider
 
 
 class Spider(Spider):
+
+    def init(self, extend=""):
+        self.ihost=self.imgsite()
+        pass
+
     def getName(self):
-        return "JianPian"
-
-    def init(self, extend):
-        self.home_url = 'https://api.ubj83.com'
-        self.headers = {
-            "User-Agent": "jianpian-android/365",
-            'Host': 'https://api.ubj83.com',
-            'JPAUTH': 'bhhxAK8WwQNOcZk9C4+mO4H+W3IdMN52/XBD3ND0yr4C',
-            'Connection': 'keep-alive',
-            'Accept-Encoding': 'gzip',
-        }
-
-    def getDependence(self):
-        return []
+        pass
 
     def isVideoFormat(self, url):
         pass
@@ -27,129 +22,126 @@ class Spider(Spider):
     def manualVideoCheck(self):
         pass
 
-    def homeContent(self, filter):
-
-        return {'class': [
-            {
-                'type_id': '1',
-                'type_name': '电影'
-            },
-            {
-                'type_id': '2',
-                'type_name': '电视剧'
-            },
-            {
-                'type_id': '3',
-                'type_name': '动漫'
-            },
-            {
-                'type_id': '4',
-                'type_name': '综艺'
-            }
-        ]}
-
-    def homeVideoContent(self):
-        a = []
-        try:
-            res = requests.get(url=self.home_url + '/api/crumb/list?code=unknownbe2a5c4162bb5528&category_id=0&area=0&year=0&limit=24&channel=wandoujia&page=1&sort=hot&type=0', headers=self.headers)
-            if res.status_code != 200:
-                return {'list': [], 'parse': 0, 'jx': 0, 'msg': f'status_code: {res.status_code}'}
-            data_list = res.json()['data']
-            for i in data_list:
-                a.append(
-                    {
-                        'vod_id': i['id'],
-                        'vod_name': i['title'],
-                        'vod_pic': i['path'],
-                        'vod_remarks': i['mask'],
-                        'vod_year': i['score'],
-                    }
-                )
-            return {'list': a, 'parse': 0, 'jx': 0}
-        except requests.exceptions.RequestException as e:
-            return {'list': [], 'parse': 0, 'jx': 0, 'msg': str(e)}
-
-    def categoryContent(self, cid, page, filter, ext):
-        a = []
-        try:
-            res = requests.get(url=self.home_url + f'/api/crumb/list?code=unknownbe2a5c4162bb5528&category_id={cid}&area=0&year=0&limit=24&channel=wandoujia&page={page}&sort=hot&type=0',
-                               headers=self.headers)
-            if res.status_code != 200:
-                return {'list': [], 'parse': 0, 'jx': 0, 'msg': f'status_code: {res.status_code}'}
-            data_list = res.json()['data']
-            for i in data_list:
-                a.append(
-                    {
-                        'vod_id': i['id'],
-                        'vod_name': i['title'],
-                        'vod_pic': i['path'],
-                        'vod_remarks': i['mask'],
-                        'vod_year': i['score'],
-                    }
-                )
-            return {'list': a, 'parse': 0, 'jx': 0}
-        except requests.exceptions.RequestException as e:
-            return {'list': [], 'parse': 0, 'jx': 0, 'msg': str(e)}
-
-
-    def detailContent(self, did):
-        ids = did[0]
-        video_list = []
-        try:
-            res = requests.get(f'{self.home_url}/api/node/detail?channel=wandoujia&token=&id={ids}', headers=self.headers)
-            if res.status_code != 200:
-                return {"list": video_list, 'parse': 0, 'jx': 0}
-            video_list.append(
-                {
-                    'type_name': ' '.join(i['name'] for i in res.json()['data']['types']),
-                    'vod_id': ids,
-                    'vod_name': res.json()['data']['title'],
-                    'vod_remarks': res.json()['data']['mask'],
-                    'vod_year': res.json()['data']['year']['title'],
-                    'vod_area': ' '.join(i['title'] for i in res.json()['data']['category']),
-                    'vod_actor': ' '.join(i['name'] for i in res.json()['data']['actors']),
-                    'vod_director': res.json()['data']['directors'][0]['name'],
-                    'vod_content': res.json()['data']['description'],
-                    'vod_play_from': '边下边播超清版',
-                    'vod_play_url': '#'.join([i['val'] for i in res.json()['data']['m3u8_downlist']])
-                }
-            )
-            return {"list": video_list, 'parse': 0, 'jx': 0}
-        except requests.RequestException as e:
-            return {'list': [], 'msg': str(e)}
-
-    def searchContent(self, key, quick, page='1'):
-        a = []
-        try:
-            res = requests.get(url=self.home_url + f'/api/video/search?page={page}&key={key}',headers=self.headers)
-            if res.status_code != 200:
-                return {'list': [], 'parse': 0, 'jx': 0, 'msg': f'status_code: {res.status_code}'}
-            data_list = res.json()['data']
-            for i in data_list:
-                a.append(
-                    {
-                        'vod_id': i['id'],
-                        'vod_name': i['title'],
-                        'vod_pic': i['tvimg'],
-                        'vod_remarks': i['mask'],
-                        'vod_year': i['score'],
-                    }
-                )
-            return {'list': a, 'parse': 0, 'jx': 0}
-        except requests.exceptions.RequestException as e:
-            return {'list': [], 'parse': 0, 'jx': 0, 'msg': str(e)}
-
-    def playerContent(self, flag, pid, vipFlags):
-        h = {
-            'User-Agent': 'jianpian-android/365',
-        }
-        return {'url': pid, 'header': h, 'parse': 0, 'jx': 0}
-
-    def localProxy(self, params):
+    def destroy(self):
         pass
 
-    def destroy(self):
-        return '正在Destroy'
+    host='https://api.ubj83.com'
 
-if __name__ == '__main__':
-    pass
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 11; M2012K10C Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36;webank/h5face;webank/1.0;netType:NETWORK_WIFI;appVersion:416;packageName:com.jp3.xg3',
+        'Accept': 'application/json, text/plain, */*',
+        'x-requested-with': 'com.jp3.xg3',
+        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    }
+
+    def imgsite(self):
+        data=self.fetch(f"{self.host}/api/appAuthConfig",headers=self.headers).json()
+        host=data['data']['imgDomain']
+        return host if host.startswith('http') else f"https://{host}"
+
+    def getfts(self,id):
+        data=self.fetch(f"{self.host}/api/crumb/filterOptions",params={'fcate_pid':id},headers=self.headers).json()
+        fts=[{
+            'key': i['key'],
+            'name':i['key'],
+            'value': [{
+                'n': j['name'],
+                'v': j['id']
+            } for j in i['data']]
+        } for i in data['data']]
+        return id,fts
+
+    def build_cl(self,data,tid=''):
+        videos=[]
+        for i in data:
+            text=json.dumps(i.get('res_categories',[]))
+            videos.append({
+                'vod_id': f"{i.get('id')}@{'67' if json.dumps('短剧') in text and '67' in text else tid}",
+                'vod_name': i.get('title'),
+                'vod_pic': f"{self.ihost}{i.get('path') or i.get('cover_image') or i.get('thumbnail')}",
+                'vod_remarks': i.get('mask'),
+                'vod_year': i.get('score'),
+            })
+        return videos
+
+    def homeContent(self, filter):
+        result = {}
+        cdata=self.fetch(f"{self.host}/api/term/home_fenlei",headers=self.headers).json()
+        hdata=self.fetch(f"{self.host}/api/dyTag/hand_data",params={'category_id':cdata['data'][0]['id']},headers=self.headers).json()
+        classes = []
+        filters = {}
+        for k in cdata['data']:
+            if 'abbr' in k:
+                classes.append({
+                    'type_name': k['name'],
+                    'type_id': k['id']
+                })
+        with concurrent.futures.ThreadPoolExecutor(max_workers=len(classes)) as executor:
+            future_to_aid = {
+                executor.submit(self.getfts, aid['type_id']): aid['type_id']
+                for aid in classes
+            }
+            for future in concurrent.futures.as_completed(future_to_aid):
+                aid = future_to_aid[future]
+                try:
+                    aid_id, fts = future.result()
+                    filters[aid_id] = fts
+                except Exception as e:
+                    print(f"Error processing aid {aid}: {e}")
+        result['class'] = classes
+        result['filters'] = filters
+        result['list'] = [item for i in hdata['data'].values() for item in self.build_cl(i)]
+        return result
+
+    def homeVideoContent(self):
+        pass
+    def categoryContent(self, tid, pg, filter, extend):
+        params={**{'fcate_pid': tid, 'page': pg}, **extend}
+        path= '/api/crumb/shortList' if tid=='67' else '/api/crumb/list'
+        data=self.fetch(f"{self.host}{path}",params=params,headers=self.headers).json()
+        result = {}
+        result['list'] = self.build_cl(data['data'],tid)
+        result['page'] = pg
+        result['pagecount'] = 9999
+        result['limit'] = 90
+        result['total'] = 999999
+        return result
+
+    def detailContent(self, ids):
+        ids=ids[0].split('@')
+        path, ikey = ('/api/detail', 'vid') if ids[-1] == '67' else ('/api/video/detailv2', 'id')
+        data=self.fetch(f"{self.host}{path}",params={ikey:ids[0]},headers=self.headers).json()
+        v=data['data']
+        if ids[-1]=='67':
+            pdata=v.get('playlist',[])
+            n,p=[pdata[0].get('source_config_name')],['#'.join([f"{i.get('title')}${i['url']}" for i in pdata])]
+        else:
+            n,p=[],[]
+            for i in v.get('source_list_source',[]):
+                n.append(i.get('name'))
+                p.append('#'.join([f"{j.get('source_name') or j.get('weight')}${j['url']}" for j in i.get('source_list',[])]))
+
+        vod = {
+            'type_name': '/'.join([i.get('name') for i in v.get('types',[])]),
+            'vod_year': v.get('year'),
+            'vod_area': v.get('area'),
+            'vod_remarks': v.get('update_cycle'),
+            'vod_actor': '/'.join([i.get('name') for i in v.get('actors',[])]),
+            'vod_content': v.get('description'),
+            'vod_play_from': '$$$'.join(n),
+            'vod_play_url': '$$$'.join(p)
+        }
+        return {'list':[vod]}
+
+    def searchContent(self, key, quick, pg="1"):
+        data=self.fetch(f"{self.host}/api/v2/search/videoV2",params={'key':key,'page':pg,'pageSize':20},headers=self.headers).json()
+        return {'list':self.build_cl(data['data']),'page':pg}
+
+    def playerContent(self, flag, id, vipFlags):
+        return  {'parse': 0, 'url': id, 'header': {'User-Agent':self.headers['User-Agent']}}
+
+    def localProxy(self, param):
+        pass
+
+    def liveContent(self, url):
+        pass
